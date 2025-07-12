2ff9e18b049452fd543f681dd0e723030c12af2b
Ziyad Ahmed <ziyafwru.12@gmail.com>
2025-07-12 11:13:31 +0300

    Implement cache invalidation for Property model using Django signals

---
+++ properties/__init__.py
@@ -0,0 +1,3 @@
+# properties/__init__.py
+
+default_app_config = 'properties.apps.PropertiesConfig'
--- properties/apps.py
+++ properties/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
+ # properties/apps.py
+
+    def ready(self):
+        import properties.signals  # noqa
