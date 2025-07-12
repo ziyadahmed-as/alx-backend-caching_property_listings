157598ce85971f7f2afbb5fb8f465a99ee29ca73
Ziyad Ahmed <ziyafwru.12@gmail.com>
2025-07-12 10:51:31 +0300

    Add Redis-cached property list view at /properties/

---
+++ properties/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import property_list
+
+urlpatterns = [
+    path('', property_list, name='property_list'),
+]
--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+from .models import Property
 
-# Create your views here.
+@cache_page(60 * 15)
+def property_list(request):
+    properties = Property.objects.all()
+    data = [
+        {
+            "id": prop.id,
+            "title": prop.title,
+            "description": prop.description,
+            "price": float(prop.price),
+            "location": prop.location,
+            "created_at": prop.created_at.isoformat(),
+        }
+        for prop in properties
+    ]
+    return JsonResponse({"properties": data})
